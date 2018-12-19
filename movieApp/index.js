e9512dc20bab5c3da3c7de1faebe86aa858653fc
Parker Sipes <parker@parkersipes.com>
2018-12-19 06:11:16 -0500

    added search function and linked it to /result for API

--- movieApp/index.js
+++ movieApp/index.js
@@ -5,11 +5,17 @@ const request          = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+	res.render("search");
+
+});
+
 app.get("/result", function(req, res){
 
 
-
-		request('http://www.omdbapi.com/?apikey=thewdb&s=california', function(error, response, body){
+		var search = req.query.search
+		var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + search
+		request(url, function(error, response, body){
 
 				if(!error && response.statusCode == 200){
 				// res.send(body);
